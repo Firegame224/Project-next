0bb77df05ea068c9c22681119427cf592f96bfd2
Firegame224 <xg474627@gmail.com>
2025-01-20 13:17:45 +0700

    Perubahan h1 di card

--- src/app/components/listanime/Card.tsx
+++ src/app/components/listanime/Card.tsx
@@ -9,7 +9,7 @@ export default function Card ({anime}: {anime : Anime}) {
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <img className="rounded-t-lg" src={anime.img} alt={anime.name} />
         <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {anime.name}
+            {anime.name} Aku kaya
         </h1>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {anime.description}
